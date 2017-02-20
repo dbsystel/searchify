a7a5e79232fd4324f94ec8355775ff6af793aaaf
Jakob Frank <jakob@apache.org>
2017-02-20 15:14:10 +0100

    Stubs for the searchify-webapp

---
+++ webapp/src/main/java/de/db/searchify/SearchifyApplication.java
@@ -0,0 +1,12 @@
+package de.db.searchify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchifyApplication.class, args);
+	}
+}
---
+++ webapp/src/test/java/de/db/searchify/SearchifyApplicationTests.java
@@ -0,0 +1,16 @@
+package de.db.searchify;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SearchifyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
